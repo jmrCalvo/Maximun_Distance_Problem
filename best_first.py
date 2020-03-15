9a6d10ca088ecb62fd33f44d37d4543fa36c69fc
Jose Manuel Rodriguez Calvo <jmr_calvo@outlook.com>
2020-03-15 03:03:38 +0100

    both solutions

---
+++ best_first.py
@@ -0,0 +1,97 @@
+import read_file
+import sys
+import random
+import objective
+import numpy as np
+import greedy
+from time import time
+import blockElements
+
+OBJETIVECOUNT=0
+
+
+def calculateNeighboursWeights(matrix,initial_pos,forbiddenElements,n_solutions):
+    neighbours=np.arange(matrix[0].size)
+    neighboursWay=[None] * matrix[0].size
+    copyMatrix=np.array(matrix)
+    copyMatrix=blockElements.block(forbiddenElements,copyMatrix)
+
+    for index in range(matrix[0].size):
+        global OBJETIVECOUNT
+        if OBJETIVECOUNT >100000:
+            print("MORE THAN 100000 CALCULATIONS OF OBJETIVE")
+            break
+        if index in forbiddenElements:
+            #print("++++++++++esta prohibido",index)
+            neighbours[index]=0
+            neighboursWay[index]=[]
+        else:
+            #print("**********esta posible",index)
+            forbiddenElements=np.append(forbiddenElements,initial_pos)
+            way=greedy.createSolution(index,copyMatrix,n_solutions)
+            neighboursWay[index]=np.insert(way,0,initial_pos)
+            neighbours[index]=objective.calculate_sum(matrix,neighboursWay[index])
+            
+            OBJETIVECOUNT=OBJETIVECOUNT+1
+
+    return(neighbours,neighboursWay)
+    
+
+def createAlternativeSolution(positionValue,matrix,forbiddenElements,oldSolution):
+    copyMatrix=np.array(matrix)
+    (neighbours,neighboursway)=calculateNeighboursWeights(copyMatrix,positionValue,forbiddenElements,oldSolution.size)
+    maximIndex=np.argmax(neighbours)
+    
+    return (neighbours[maximIndex],neighboursway[maximIndex])
+
+
+def createSolution(initial,matrix,noSolutions):
+    solutionActual=greedy.createSolution(initial,matrix,noSolutions)
+    
+    #the index is from the start until the length of the array minus one,
+    #because the last movement to be changed is the minus two with can
+    #select just the maximun
+    
+    for index in range(len(solutionActual)-2):
+        global OBJETIVECOUNT
+        if OBJETIVECOUNT >100000:
+            break
+        oldWeight=objective.calculate_sum(matrix,solutionActual[index:])
+        OBJETIVECOUNT=OBJETIVECOUNT+1
+        
+        (weight,neighboursway)=createAlternativeSolution(solutionActual[index],matrix,solutionActual[:index],solutionActual[index+1:])
+        if(weight>oldWeight):
+            solutionActual=np.concatenate((solutionActual[:index], neighboursway))
+
+    return solutionActual
+
+
+
+def main():
+    #the initial
+    SEED = int(sys.argv[1])
+    file=str(sys.argv[2])
+    #read of the file
+    matrix,n_solutions = read_file.create_array(file)
+
+    #create the init element
+    random.seed(SEED)
+    init=random.randint(0,matrix[0].size)
+
+    start_time = time()
+    solution = createSolution(init,matrix,n_solutions)
+    elapsed_time = time() - start_time
+
+
+    #print("la suma fue",objective.calculate_sum(solution,matrix))
+    print(solution)
+    print(objective.calculate_sum(matrix,solution))
+    print("el tiempo fue",elapsed_time)
+
+
+
+OBJETIVECOUNT=0
+
+if __name__ =="__main__":
+    main()
+
---
+++ blockElements.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def block(arrayForbidenElements, matrix):
+    copyMatrix=np.array(matrix)
+    if len(arrayForbidenElements)==0:
+        return matrix
+    for element in arrayForbidenElements:
+        copyMatrix[:,element]=0
+        copyMatrix[element,:]=0
+    return copyMatrix
