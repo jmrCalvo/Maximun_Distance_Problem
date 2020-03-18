e03eb2786e106248161388395094f2758c20901c
Jose Manuel Rodriguez Calvo <jmr_calvo@outlook.com>
2020-03-18 10:50:24 +0100

    ➕the sumatory of the vector in a matrix space

--- objective.py
+++ objective.py
@@ -1,6 +1,7 @@
 def calculate_sum(matrix, vector):
     pos_i=int(vector[0])
     sum_all=0
+    
     for pos_j in vector:
         sum_all=sum_all+matrix[int(pos_i)][int(pos_j)]
         pos_i=pos_j
